96f1679a9baade9500ba357588a54fd70bfc0a92
augusto <augustoal@gmail.com>
2019-03-21 16:24:31 -0300

    Hechos los detalles de peliculas y personas, falta cambiar los links por nombres

--- api/controllers/detalle.js
+++ api/controllers/detalle.js
@@ -18,12 +18,41 @@ module.exports = {
     }
   },
 
-
   fn: async function (inputs, exits) {
-    var elemento = this.req.param('elemento');
-    // All done.
-    return exits.success({nombre: elemento});
 
+    function encontrar_nombre(url, tipo){
+      var request = require('request');
+      request.get({url: url}, function(error, respuesta) {
+        if (error) {
+          console.log(error);
+        }
+        else {
+          var informacion = JSON.parse(respuesta.body);
+          if (tipo =='films'){
+            return informacion.title;
+          } else if (tipo == 'people'){
+            return informacion.name;
+          }
+        }
+      });
+    }
+
+
+    var elemento = this.req.param('elemento').split(",");
+    var tipo = elemento[0];
+    var id = elemento[1];
+    console.log(`Tipo : ${tipo}\nID: ${id}`);
+    var request = require('request');
+    request.get({url: `https://swapi.co/api/${tipo}/${id}`}, function(error, respuesta) {
+      if (error) {
+        console.log(error);
+        return exits.success({tipo: "Error"});
+      }
+      else {
+        var informacion = JSON.parse(respuesta.body);
+        return exits.success({tipo: tipo, id: id, informacion: informacion});
+      }
+    });
   }
 
 
