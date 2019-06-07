dce19464ce615f17788142e3c5de7ed14de67133
augusto <augustoal@gmail.com>
2019-06-06 23:09:44 -0400

    Tarea lista con graphql

--- api/controllers/detalle.js
+++ api/controllers/detalle.js
@@ -78,9 +78,112 @@ module.exports = {
       graphqlRequest.request('https://swapi-graphql-integracion-t3.herokuapp.com', query).then(
         function (data) {return exits.success({tipo: tipo, id: id, informacion:data.film})}
       );
-    } else if (tipo = 'people') {
-      
-    }
+    } else if (tipo == 'people') {
+      const query = `{
+                      person(id:"${id}"){
+                        name
+                        birthYear
+                        eyeColor
+                        gender
+                        hairColor
+                        height
+                        mass
+                        id
+                        skinColor
+                        created
+                        edited
+                        homeworld{
+                          name
+                          id
+                        }
+                        filmConnection{
+                          films{
+                            title
+                            episodeID
+                          }
+                        }
+                        starshipConnection{
+                          starships{
+                            name
+                            id
+                          }
+                        }
+                      }
+                    }`;
+      graphqlRequest.request('https://swapi-graphql-integracion-t3.herokuapp.com', query).then(
+        function (data) {return exits.success({tipo: tipo, id: id, informacion:data.person})}
+      );
+    } else if (tipo == 'planets') {
+      const query = `{
+                      planet(id:"${id}"){
+                        name
+                        diameter
+                        rotationPeriod
+                        orbitalPeriod
+                        gravity
+                        population
+                        id
+                        climates
+                        terrains
+                        surfaceWater
+                        created
+                        edited
+                        residentConnection{
+                          residents{
+                            name
+                            id
+                          }
+                        }
+                        filmConnection{
+                          films{
+                            title
+                            episodeID
+                          }
+                        }
+                      }
+                    }`;
+      graphqlRequest.request('https://swapi-graphql-integracion-t3.herokuapp.com', query).then(
+        function (data) {return exits.success({tipo: tipo, id: id, informacion:data.planet})}
+      );
+    } else if (tipo == 'starships') {
+      const query = `{
+                      starship(id:"${id}"){
+                        name
+                        model
+                        starshipClass
+                        manufacturers
+                        costInCredits
+                        length
+                        crew
+                        passengers
+                        maxAtmospheringSpeed
+                        hyperdriveRating
+                        MGLT
+                        cargoCapacity
+                        consumables
+                        created
+                        edited
+                        id
+                        pilotConnection{
+                          pilots{
+                            id
+                            name
+                          }
+                        }
+                        filmConnection{
+                          films{
+                            title
+                            episodeID
+                          }
+                        }
+                      }
+                    }`;
+      graphqlRequest.request('https://swapi-graphql-integracion-t3.herokuapp.com', query).then(
+        function (data) {return exits.success({tipo: tipo, id: id, informacion:data.starship})}
+      );
+    } else {
+      return exits.success({tipo: tipo, id: id, informacion:null});
+    };
 
 
     // var request = require('request');
